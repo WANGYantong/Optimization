0064c197459cbf15ad2d29178dd45a4865624d4c
Yantong WANG <yantong.wang@kcl.ac.uk>
2017-12-06 19:46:15 +0000

    generate the network topology

--- Delay Model/main.m
+++ Delay Model/main.m
@@ -1,7 +1,30 @@
 clear
 clc
 
+rng(2);
 % generate network topology
+s=[1,1,2,2,3,3,5];
+t=[2,3,4,5,5,6,6];
+weights=10*randi([1,10],size(s));
+names={'ec1','n1','ec2','ec3',...
+    'ec4','n2'};
+
+hold on;
+G=graph(s,t,weights,names);
+LWidths=3*G.Edges.Weight/max(G.Edges.Weight);
+p=plot(G,'EdgeLabel',G.Edges.Weight,'NodeLabel',...
+    G.Nodes.Name,'LineWidth',LWidths);
+p.Marker='o';
+p.NodeColor='r';
+p.MarkerSize=8;
+p.EdgeColor='b'; 
+p.LineStyle='--';
+p.XData=[3,2,4,1,3,5];
+p.YData=[3,2,2,1,1,1];
+plot(3,1,'black*');
+hold off;
+title('Network Topology');
+
 
 % parameters
 
