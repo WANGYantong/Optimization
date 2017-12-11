11812f568710fcc85858ba400787dfb023ec01a5
Yantong WANG <yantong.wang@kcl.ac.uk>
2017-12-11 17:23:43 +0000

    add a method to find the path and link set

--- Delay Model/main.m
+++ Delay Model/main.m
@@ -2,50 +2,77 @@ clear
 clc
 
 rng(2);
+
 %%%%%%%%%%%%%%%%%%%%%%% generate network topology %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-s=[1,1,2,2,3,3,5];
-t=[2,3,4,5,5,6,6];
-weights=10*randi([1,10],size(s));
+% the set of flows in the netwrok
+flow={'flow1','flow2'}; %$$%
+N=length(flow);
+for v=1:N
+    eval([flow{v},'=',num2str(v),';']);
+end
+
+% the nodes and edge clouds in the network 
 names={'ec1','n1','ec2','ec3',...
-    'ec4','n2'};
+    'ec4','n2'}; %$$%
 N=length(names);
 for v=1:N
     eval([names{v},'=',num2str(v),';']);
 end
-node=[n1,n2];
-edgecloud=[ec1,ec2,ec3,ec4];
+node=[n1,n2]; %$$%
+edgecloud=[ec1,ec2,ec3,ec4]; %$$%
 
-hold on;
-G=graph(s,t,weights,names);
-LWidths=3*G.Edges.Weight/max(G.Edges.Weight);
-p=plot(G,'EdgeLabel',G.Edges.Weight,'NodeLabel',...
-    G.Nodes.Name,'LineWidth',LWidths);
-p.Marker='o';
-p.MarkerSize=8;
-p.EdgeColor='b'; 
-p.LineStyle='--';
-highlight(p,edgecloud,'nodecolor','r');
-highlight(p,ec4,'nodecolor','g'); %the original edge cloud
-p.XData=[3,2,4,1,3,5];
-p.YData=[3,2,2,1,1,1];
-hold off;
-title('Network Topology');
+% generate the undirected graph
+s=[1,1,2,2,3,3,5]; %$$%
+t=[2,3,4,5,5,6,6]; %$$%
+for ii=1:length(flow)
+    weights{ii}=10*randi([1,10],size(s));
+    G{ii}=graph(s,t,weights{ii},names);
+end
+
+% plot each flow graph
+figure;
+cxd=['r','b','g','c','y','m','k'];
+for ii=1:length(flow)
+    subplot(2,1,ii);
+    LWidths{ii}=3*G{ii}.Edges.Weight/max(G{ii}.Edges.Weight);
+    p(ii)=plot(G{ii},'EdgeLabel',G{ii}.Edges.Weight,'NodeLabel',...
+        G{ii}.Nodes.Name,'LineWidth',LWidths{ii});
+    p(ii).Marker='o';
+    p(ii).MarkerSize=8;
+    p(ii).EdgeColor=cxd(randi(length(cxd))); 
+    p(ii).LineStyle='--';
+    highlight(p(ii),edgecloud,'nodecolor','r');
+    highlight(p(ii),ec4,'nodecolor','g'); %the original edge cloud
+    p(ii).XData=[3,2,4,1,3,5]; 
+    p(ii).YData=[3,2,2,1,1,1]; 
+    title(['Flow',num2str(ii)]);
+end
+suptitle('Network Topology');
+
 %%%%%%%%%%%%%%%%%%%%% parameters %%%%%%%%%%%%%%%%%%%%%%%%%%
-% generate route cost matrix
-% this is the shortest path between sources and targets. 
-% find a way to get all the possible paths between two nodes
+% the set of links
+link1=G{flow1}.Edges;
+link2=G{flow2}.Edges;
+
+% the set of paths
+% the paths for flow1 and flow2 are same 
 sources=[ec1,ec2,ec3,ec4];
 targets=[ec3,ec4,n2];
-wkp=distances(G,sources,targets);
+path=cell(length(sources),length(targets));
+for ii=1:length(sources)
+    for jj=1:length(targets)
+        path{ii,jj}=GetPathBetweenNode(G{1},'undirected',sources(ii),...
+            targets(jj));
+    end
+end
 
-K=4; % number of flows
-E=4; % number of edge clouds
-%P=; % number of paths
-%L=; % number of links
+% path cost OR routing cost
 
 
+% matrix for path cross edge cloud
 
-% decision variable
+
+%%%%%%%%%%%%%%%%%%%%%%%% decision variable %%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % constraints
 
